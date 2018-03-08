935711dddabb5904019a9ac5a57d4d77a52a569f
Oczane <oczane.rivera@gmail.com>
2018-03-07 20:26:19 -0500

    Add config parameteres for the APP's dimensions

--- src/js/store.js
+++ src/js/store.js
@@ -14,6 +14,10 @@ const SHOW_COMPLETE_ITEMS = true
 const COLOR_ODD_NUMBER_TASK_HOLDERS = false
 const NEW_TASKS_AT_TOP = true
 const NEW_TASKS_AT_BOTTOM = false
+const APP_MAX_WIDTH = 100 // percentage
+const APP_MAX_HEIGHT = 100 // percentage
+const APP_MIN_WIDTH = 375 // pixels
+const APP_MIN_HEIGHT = 400 // pixels
 
 //*** STATE */
 const state = {
@@ -48,24 +52,27 @@ function setList (listArray) {
 
 function addToList (task) {
   state.list.push(task)
-  let newTaskHolder = createAnElem('li')
-  let newTask = createAnElem('p', task.value)
+  let newTaskHolder = createDomElem('li')
+  let newTask = createDomElem('p', task.value)
   newLi.appendChild(newTask)
   listContainer.appendChild(newTaskHolder)
 }
 
 function removeFromList (task) {
-  let index = state.list.indexOf(task)
-  let element = taskContainers[index]
-  listContainer.removeChild(element)
+  removeDomElem(state.list.indexOf(task))
   state.list.splice(index, 1)
 }
 
 //*** DOM MANIPULATION */
 
-function createAnElem (elemType, text||'') {
+function createDomElem (elemType, text='') {
   let newElem = document.createElement(elemType)
   let newTextNode = document.createTextNode(text)
   newElem.appendChild(newTextNode)
   return newElem
+}
+
+function removeDomElem (elemIndex) {
+  let element = taskContainers[elemIndex]
+  listContainer.removeChild(element)
 }
