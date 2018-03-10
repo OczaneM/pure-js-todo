d0690797570c4be6df5b579c4f05d0d5d2f5f03b
Oczane <oczane.rivera@gmail.com>
2018-03-10 08:37:39 -0500

    Add checkActiveTab to keep track of tab styling

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,24 +1,48 @@
 'use strict'
 
 const Nav = {
+
   render: function () {
     app.appendChild(
       header({className: 'nav-header'}, null,
       h3(null, 'TODO'),
       nav(null, null,
         ul(null, null,
-          li({className: 'active'}, null,
+          li({id: 'createNav'}, null,
             a({href: ''}, 'create')
           ),
-          li({className: 'active'}, null,
+          li({id: 'searchNav'}, null,
             a({href: ''}, 'search')
           ),
-          li({className: 'active'}, null,
+          li({id: 'configureNav'}, null,
             a({href: ''}, 'configure')
           )
          )
         )
       )
     )
+    this.checkActiveTab()
+  },
+
+  checkActiveTab: function () {
+    switch (VIEW) {
+      case 'createTab':
+          document.getElementById('createNav').className = 'active'
+          document.getElementById('searchNav').className = 'tab-control'
+          document.getElementById('configureNav').className = 'tab-control'
+          break
+      case 'searchTab':
+          document.getElementById('createNav').className = 'tab-control'
+          document.getElementById('searchNav').className = 'active'
+          document.getElementById('configureNav').className = 'tab-control'
+          break
+      case 'configure':
+          document.getElementById('createNav').className = 'tab-control'
+          document.getElementById('searchNav').className = 'tab-control'
+          document.getElementById('configureNav').className = 'active'
+          break
+      default:
+          break
+    }
   }
 }
